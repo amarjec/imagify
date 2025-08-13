613e58743029342d36c28b92768154a809496413
Amar Agrawal <159050763+amarjec@users.noreply.github.com>
2025-08-13 12:40:39 +0530

    Update BuyCredit.jsx

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -83,7 +83,7 @@ const BuyCredit = () => {
             <p className='mt-3 mb-1 font-semibold'>{item.id}</p>
             <p className='text-sm'>{item.desc}</p>
             <p className='mt-6'>
-              <span className='text-3xl font-medium'>  ₹{item.price}</span> / {item.credits} credits</p>
+              <span className='text-3xl font-medium'>  ${item.price}</span> / {item.credits} credits</p>
 
               <button
               onClick={() => paymentRazorpay(item.id)}
